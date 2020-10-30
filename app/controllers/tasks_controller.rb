e0431d21776b6ae5c261121d1241e840a16796f4
uk_tech <spring_p0012015@yahoo.co.jp>
2020-10-30 07:26:50 +0000

    fix_mentor_review3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,10 +51,7 @@ class TasksController < ApplicationController
   end
 
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
+ 
   
   def task_params
     params.require(:task).permit(:content, :status)
