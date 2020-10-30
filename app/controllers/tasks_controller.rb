4ae4342ae7175b73739da97705878d83789f7b8f
uk_tech <spring_p0012015@yahoo.co.jp>
2020-10-30 07:06:42 +0000

    fix_mentor_review2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    if logged_in?
-      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc)
-    end
   end
 
   def show
